9722fe4dc106cdd51bb54f967baf2e4074f5c5c1
unknown <jazminalmaza09@gmail.com>
2026-06-30 15:08:01 -0300

    Corrigiendo index

--- editar_asistencia.php
+++ editar_asistencia.php
@@ -19,14 +19,9 @@ if(isset($_POST['guardar'])){
         WHERE id='$id'"
     );
 
-<<<<<<< HEAD
    header("Location: ver_asistencia.php");
 exit;
-=======
-    echo "<h3>Asistencia actualizada</h3>";
->>>>>>> 9f117a7 (primer commit jijijija)
 }
-
 $sql = "
 SELECT
 a.*,
