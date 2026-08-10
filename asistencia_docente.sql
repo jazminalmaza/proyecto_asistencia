7955a24a873645804ed3cb1e9ffc810a3fe2b7b8
sofiiilencina <sofiaepet20@gmail.com>
2026-08-10 14:35:59 -0300

    Se modificó la base de datos

--- asistencia_docente.sql
+++ asistencia_docente.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-08-2026 a las 19:02:59
+-- Tiempo de generación: 10-08-2026 a las 19:34:32
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,23 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `asistencia`
---
-
-CREATE TABLE `asistencia` (
-  `id_asistencia` int(11) NOT NULL,
-  `fecha` date NOT NULL,
-  `nombre_docente` varchar(100) NOT NULL,
-  `materia` varchar(70) NOT NULL,
-  `turno` varchar(20) NOT NULL,
-  `hora_ingreso` time NOT NULL,
-  `hora_egreso` time NOT NULL,
-  `estado` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `docentes`
 --
 
@@ -49,106 +32,24 @@ CREATE TABLE `docentes` (
   `DNI` int(10) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
-  `teléfono` int(20) NOT NULL,
-  `email` varchar(50) NOT NULL
+  `teléfono` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `id_huella` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `docentes`
 --
 
-INSERT INTO `docentes` (`id_docente`, `DNI`, `nombre`, `apellido`, `teléfono`, `email`) VALUES
-(5, 48512687, 'Guadalupe', 'Medel', 2147483647, 'guada@gmail.com'),
-(6, 48749707, 'Sofia', 'Lencina', 2147483647, 'sofiaepet20@gmail.com'),
-(7, 48749707, 'Sofia', 'Lencina', 2147483647, 'sofiaepet20@gmail.com');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `docentes_materias`
---
-
-CREATE TABLE `docentes_materias` (
-  `id_docente` int(70) NOT NULL,
-  `id_materia` int(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `jefes_de_preceptores`
---
-
-CREATE TABLE `jefes_de_preceptores` (
-  `id_jefe` int(11) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `contraseña` varchar(50) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
-  `dni` int(10) NOT NULL,
-  `turno` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `jefe_modifica_asistencia`
---
-
-CREATE TABLE `jefe_modifica_asistencia` (
-  `id_jefe` int(11) NOT NULL,
-  `id_asistencia` int(11) NOT NULL,
-  `fecha_modificacion` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `materia`
---
-
-CREATE TABLE `materia` (
-  `id_materia` int(11) NOT NULL,
-  `nombre` varchar(70) NOT NULL,
-  `turno` varchar(20) NOT NULL,
-  `curso` varchar(10) NOT NULL,
-  `horario_inicio` time NOT NULL,
-  `horario_finalizacion` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `materia`
---
-
-INSERT INTO `materia` (`id_materia`, `nombre`, `turno`, `curso`, `horario_inicio`, `horario_finalizacion`) VALUES
-(2, 'Dibujo', 'Mañana', '1° 1°', '01:30:00', '03:00:00'),
-(3, 'Inglés', 'Vespertino', '4° 1°', '03:40:00', '04:40:00'),
-(4, 'Inglés', 'Mañana', '3° 3°', '16:00:00', '17:20:00');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `prosecretario`
---
-
-CREATE TABLE `prosecretario` (
-  `id_prosecretario` int(11) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `contraseña` varchar(50) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
-  `dni` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `docentes` (`id_docente`, `DNI`, `nombre`, `apellido`, `teléfono`, `email`, `id_huella`) VALUES
+(5, 48512687, 'Guadalupe', 'Medel', '2147483647', 'guada@gmail.com', NULL),
+(6, 48749707, 'Sofia', 'Lencina', '2147483647', 'sofiaepet20@gmail.com', NULL),
+(7, 48749707, 'Sofia', 'Lencina', '2147483647', 'sofiaepet20@gmail.com', NULL),
+(8, 0, '', '', '0', '', NULL);
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `asistencia`
---
-ALTER TABLE `asistencia`
-  ADD PRIMARY KEY (`id_asistencia`);
 
 --
 -- Indices de la tabla `docentes`
@@ -157,68 +58,14 @@ ALTER TABLE `docentes`
   ADD PRIMARY KEY (`id_docente`);
 
 --
--- Indices de la tabla `docentes_materias`
---
-ALTER TABLE `docentes_materias`
-  ADD PRIMARY KEY (`id_materia`);
-
---
--- Indices de la tabla `jefes_de_preceptores`
---
-ALTER TABLE `jefes_de_preceptores`
-  ADD PRIMARY KEY (`id_jefe`);
-
---
--- Indices de la tabla `jefe_modifica_asistencia`
---
-ALTER TABLE `jefe_modifica_asistencia`
-  ADD PRIMARY KEY (`id_jefe`);
-
---
--- Indices de la tabla `materia`
---
-ALTER TABLE `materia`
-  ADD PRIMARY KEY (`id_materia`);
-
---
--- Indices de la tabla `prosecretario`
---
-ALTER TABLE `prosecretario`
-  ADD PRIMARY KEY (`id_prosecretario`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `asistencia`
---
-ALTER TABLE `asistencia`
-  MODIFY `id_asistencia` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `docentes`
 --
 ALTER TABLE `docentes`
-  MODIFY `id_docente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `jefes_de_preceptores`
---
-ALTER TABLE `jefes_de_preceptores`
-  MODIFY `id_jefe` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `materia`
---
-ALTER TABLE `materia`
-  MODIFY `id_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `prosecretario`
---
-ALTER TABLE `prosecretario`
-  MODIFY `id_prosecretario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_docente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
