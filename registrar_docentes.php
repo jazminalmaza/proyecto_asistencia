a62c2b9a73bc6e802848decad1e7809bbb93c5d6
sofiiilencina <sofiaepet20@gmail.com>
2026-08-10 15:35:08 -0300

    Se modificó la página de registrar docente y se le agregó funcionalidad de poder agregar y eliminar horarios para el docente

--- registrar_docentes.php
+++ registrar_docentes.php
@@ -8,44 +8,46 @@ if(isset($_POST['guardar'])){
     $apellido = $_POST['apellido'];
     $telefono = $_POST['telefono'];
     $email = $_POST['email'];
-    $id_huella = $_POST['id_huella'];
+    $id_huella = !empty($_POST['id_huella']) ? $_POST['id_huella'] : "NULL";
     
-    $sql = "INSERT INTO docentes (DNI , nombre, apellido, teléfono, email) VALUES ( '$dni' , '$nombre' , '$apellido' , '$telefono' , '$email' )";
-
+    $sql = "INSERT INTO docentes (DNI, nombre, apellido, teléfono, email, id_huella) 
+                VALUES ('$dni', '$nombre', '$apellido', '$telefono', '$email', $id_huella)";
+    
     if(mysqli_query($conexion, $sql)){
 
-       $id_docente_creado = mysqli_insert_id($conexion);
         $id_docente_creado = mysqli_insert_id($conexion);
 
-       if(!empty($id_huella)){
-            $fecha_actual = date("Y-m-d");
-            mysqli_query($conexion, "INSERT INTO huella_digital (id_huella, fecha_registro) VALUES ('$id_huella', '$fecha_actual')");
-        }
+            if(isset($_POST['materia']) && is_array($_POST['materia'])){
+                for($i = 0; $i < count($_POST['materia']); $i++){
 
-        if(isset($_POST['materia']) && is_array($_POST['materia'])){
-            for($i = 0; $i < count($_POST['materia']); $i++){
+                    $nom_materia = $_POST['materia'][$i];
+                    $turno = $_POST['turno'][$i];
+                    $curso = $_POST['curso'][$i];
+                    $division = $_POST['division'][$i];
+                    $curso_completo = $curso . " " . $division;
+                    $entrada = $_POST['entrada'][$i];
+                    $salida = $_POST['salida'][$i];
 
-                $nom_materia = $_POST['materia'][$i];
-                $turno = $_POST['turno'][$i];
-                $curso = $_POST['curso'][$i];
-                $division = $_POST['division'][$i];
-                $curso_completo = $curso . " " . $division;
-                $entrada = $_POST['entrada'][$i];
-                $salida = $_POST['salida'][$i];
+                    if(!empty($nom_materia)){
+                        $sql_materia = "INSERT INTO materia (nombre, turno, curso, horario_inicio, horario_finalizacion) 
+                                        VALUES ('$nom_materia', '$turno', '$curso_completo', '$entrada', '$salida')";
 
-                if(!empty($nom_materia)){
-                    $sql_materia = "INSERT INTO materia (nombre, turno, curso, horario_inicio, horario_finalizacion) VALUES ('$nom_materia', '$turno', '$curso_completo', '$entrada', '$salida')";
+                        if(mysqli_query($conexion, $sql_materia)){
+                            $id_materia_creada = mysqli_insert_id($conexion);
 
-                    mysqli_query($conexion, $sql_materia);
+                            $sql_relacion = "INSERT INTO docentes_materias (id_docente, id_materia) 
+                                            VALUES ('$id_docente_creado', '$id_materia_creada')";
+                            mysqli_query($conexion, $sql_relacion);
+                        }
+                    }
                 }
             }
-        }
 
-        echo "<p style='color: green;'>Docente, huella y horarios registrados con éxito.</p>";
-    } else {
-        echo "<p style='color: red;'>Error al guardar: " . mysqli_error($conexion) . "</p>";
+            echo "<p style='color: green;'>Docente y horarios registrados con éxito.</p>";
+        } else {
+            echo "<p style='color: red;'>Error al guardar docente: " . mysqli_error($conexion) . "</p>";
+        }
     }
-}
 ?>
 
 <!DOCTYPE html>
@@ -108,72 +110,108 @@ if(isset($_POST['guardar'])){
         </div>
         <h2>Horarios</h2>
 
-<div class="fila">
+        <div id="contenedor-horarios">
+            <div class="bloque-horario">
 
-    <div class="campo">
-        <label>Materia</label>
-        <input type="text" name="materia[]">
-    </div>
+                <div class="header-bloque">
+                    <span class="titulo-bloque">Horario</span>
+                    <button type="button" class="btn-eliminar" onclick="eliminarHorario(this)">
+                        <i class="fa-solid fa-trash"></i> Eliminar
+                    </button>
+                </div>
 
-    <div class="campo">
-        <label>Turno</label>
-         <select name="turno[]">
-            <option value="Mañana">Mañana</option>
-            <option value="Tarde">Tarde</option>
-            <option value="Vespertino">Vespertino</option>
-        </select>
-     </div>
+                <div class="fila">
 
-    <div class="campo">
-        <label>Curso</label>
-        <select name="curso[]">
-            <option>1°</option>
-            <option>2°</option>
-            <option>3°</option>
-            <option>4°</option>
-            <option>5°</option>
-            <option>6°</option>
-        </select>
-    </div>
+                    <div class="campo">
+                        <label>Materia</label>
+                        <input type="text" name="materia[]">
+                    </div>
 
-    <div class="campo">
-        <label>División</label>
-        <select name="division[]">
-            <option>1°</option>
-            <option>2°</option>
-            <option>3°</option>
-            <option>4°</option>
-            <option>5°</option>
-            <option>6°</option>
-        </select>
-    </div>
+                    <div class="campo">
+                        <label>Turno</label>
+                        <select name="turno[]">
+                            <option value="Mañana">Mañana</option>
+                            <option value="Tarde">Tarde</option>
+                            <option value="Vespertino">Vespertino</option>
+                        </select>
+                    </div>
 
+                    <div class="campo">
+                        <label>Curso</label>
+                        <select name="curso[]">
+                            <option>1°</option>
+                            <option>2°</option>
+                            <option>3°</option>
+                            <option>4°</option>
+                            <option>5°</option>
+                            <option>6°</option>
+                        </select>
+                    </div>
+
+                    <div class="campo">
+                        <label>División</label>
+                        <select name="division[]">
+                            <option>1°</option>
+                            <option>2°</option>
+                            <option>3°</option>
+                            <option>4°</option>
+                            <option>5°</option>
+                            <option>6°</option>
+                        </select>
+                    </div>
+
+                </div>
+
+                <div class="fila">
+
+                    <div class="campo">
+                        <label>Entrada</label>
+                        <input type="time" name="entrada[]">
+                    </div>
+
+                    <div class="campo">
+                        <label>Salida</label>
+                        <input type="time" name="salida[]">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <br>
+        <button type="button" id="btn-agregar">+ Agregar horario</button>
+
+        <br><br>
+
+        <button type="submit" name="guardar"> Guardar docente </button>
+
+    </form>
+</div>
 </div>
 
-<div class="fila">
+<script>
+document.getElementById('btn-agregar').addEventListener('click', function() {
+    var contenedor = document.getElementById('contenedor-horarios');
+    var primerBloque = document.querySelector('.bloque-horario');
+    
+    var nuevoBloque = primerBloque.cloneNode(true);
+    
+    var inputs = nuevoBloque.querySelectorAll('input');
+    inputs.forEach(function(input) {
+        input.value = '';
+    });
+    
+    contenedor.appendChild(nuevoBloque);
+});
 
-    <div class="campo">
-        <label>Entrada</label>
-        <input type="time" name="entrada[]">
-    </div>
+function eliminarHorario(boton) {
+    var bloque = boton.closest('.bloque-horario');
+    var totalBloques = document.querySelectorAll('.bloque-horario').length;
+    if (totalBloques > 1) {
+        bloque.remove();
+    }
+}
+</script>
 
-    <div class="campo">
-        <label>Salida</label>
-        <input type="time" name="salida[]">
-    </div>
-
-</div>
-
-<button type="button">+ Agregar horario</button>
-
-<br><br>
-
-<button type="submit" name="guardar">
-    Guardar docente
-</button>
-
-</div>
-</form>
-</div>
 </body>
 </html>
