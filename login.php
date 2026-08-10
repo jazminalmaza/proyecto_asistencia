f850e85f717bccc3b033a7b0146538935182f07c
juli <julianaluz.ledesma@icloud.com>
2026-08-10 16:42:08 -0300

    Agregando login.php

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio de sesión</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <img src="logo_epet.png" alt="Logo EPET N°20" class="logo">
+
+    <div class="heading">Sistema de Control de Asistencia</div>
+
+   <form class="form" action="" method="POST">
+
+   <input placeholder="Usuario" id="usuario" name="usuario" type="text"class="input" required>
+   <input placeholder="Contraseña"id="contraseña"name="contraseña"type="password"class="input"required>
+   
+    <input value="Iniciar Sesión" type="submit" class="login-button"/>
+
+</form>
+
+</div>
+
+</body>
+</html>
