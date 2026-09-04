f52b8e931ad05cb44a86c59ef70c788263151a84
unknown <jazminalmaza09@gmail.com>
2026-09-04 02:32:47 -0300

    Cambiando el color a aviso de Salida Registrada

--- resources/views/asistencia/registrar.blade.php
+++ resources/views/asistencia/registrar.blade.php
@@ -5,7 +5,10 @@
 @section('content')
 <div class="card-consulta card-asistencia">
     @if(session('exito'))
-        <div class="alerta exito">
+        @php
+            $esSalida = \Illuminate\Support\Str::contains(strtolower(session('exito')), 'salida');
+        @endphp
+        <div class="alerta {{ $esSalida ? 'salida' : 'exito' }}">
             <i class="fa-solid fa-circle-check"></i> {{ session('exito') }}
         </div>
     @endif
@@ -48,4 +51,12 @@
         </div>
     </form>
 </div>
+
+<style>
+    .alerta.salida {
+        background-color: #fff3cd;
+        color: #664d03;
+        border: 1px solid #ffecb5;
+    }
+</style>
 @endsection
